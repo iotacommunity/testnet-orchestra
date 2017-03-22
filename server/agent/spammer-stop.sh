b08a0d71cf829195690940d4c2759d4d1a556318
Alfred Keller <alfred.keller@centosa.ch>
2017-03-22 22:17:50 +0100

    Update spammer-stop.sh

--- server/agent/spammer-stop.sh
+++ server/agent/spammer-stop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
+. env.conf
+cd $IOTA_HOME/IOTA-ValueSpammer
+touch stop-spam
 pid=`ps -ef | grep spammer-start.sh | awk '{print $2}'`
 kill -9 $pid
 pid=`ps -ef | grep nodejs | grep spammer.js | awk '{print $2}'`
 kill -9 $pid
+
